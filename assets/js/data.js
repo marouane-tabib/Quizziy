2de9ba4367867c19162565286de62e05f52484db
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-21 17:06:19 +0100

    add data.js script file

---
+++ assets/js/data.js
@@ -0,0 +1,20 @@
+$questions = [
+    {
+        question : "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
+        options : [
+            "Amazon EC2 costs are billed on a monthly basis",
+            "Users retain full administrative access to their Amazon EC2 instances.",
+            "Amazon EC2 instances can be launched on demand when needed.",
+            "Users can permanently run enough instances to handle peak workloads",
+        ]
+    },
+    {
+        question : "Which AWS service would simplify the migration of a database to AWS?",
+        options : [
+            "AWS Storage Gateway",
+            "AWS Database Migration Service (AWS DMS).",
+            "Amazon EC2",
+            "Amazon AppStream 2.0",
+        ]
+    }
+]
