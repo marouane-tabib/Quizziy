815789e44943d6b310e765d3f13928e05ced52d2
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-30 11:36:23 +0100

    add questions.php file , and add question methods - workout

---
+++ classes/questions.php
@@ -0,0 +1,17 @@
+<?php
+require 'includes/connect.php';
+
+class question {
+
+// Properties
+    // 
+
+// Methods
+    function set_name($name) {
+        // 
+    }
+    function get_name() {
+        // return ...
+    }
+}
+?>
