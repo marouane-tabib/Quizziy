90fceff35cfabadff6d56026598f6026ba7c474b
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-02 14:39:42 +0100

    add show question method - workout

--- classes/questions.php
+++ classes/questions.php
@@ -1,18 +1,18 @@
 <?php
-require 'includes/connect.php';
+include 'includes/connect.php';
 
-class question {
-
-// Properties
-    private $title;
-    private $options;
-
-// Methods
-    function set_question($title , $options) {
-        // 
-    }
-    function get_question() {
-        // return ...
+class question extends DB {
+  // Methods
+    public function show() {
+      try {
+          $sql = "SELECT * FROM `questions` WHERE id = 3";
+          $stm = $this->pdo->prepare($sql);
+          $stm->execute();
+          $result = $stm->fetchAll();
+          return $result;
+      } catch (PDOException $e) {
+          "Erreur" . $e->getMessage();
+      }
     }
 }
 ?>
